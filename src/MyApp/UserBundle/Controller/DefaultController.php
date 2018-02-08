c8d489d575a28af917eeff810ed836735b3b4d7c
saif220 <35578951+saif220@users.noreply.github.com>
2018-02-08 09:57:57 +0100

    integration template+generation entity+fos

--- src/MyApp/UserBundle/Controller/DefaultController.php
+++ src/MyApp/UserBundle/Controller/DefaultController.php
@@ -13,7 +13,27 @@ class DefaultController extends Controller
 
     public function ContactAction()
     {
-        return $this->render('MyAppUserBundle:Contact:Contact.html.twig');
+        return $this->render('MyAppUserBundle:Pages:Contact.html.twig');
+    }
+
+    public function DoctorAction()
+    {
+        return $this->render('MyAppUserBundle:Pages:Doctor.html.twig');
+    }
+
+    public function BlogAction()
+    {
+        return $this->render('MyAppUserBundle:Pages:Blog.html.twig');
+    }
+
+    public function ShopAction()
+    {
+        return $this->render('MyAppUserBundle:Pages:Shop.html.twig');
+    }
+
+    public function EventsAction()
+    {
+        return $this->render('MyAppUserBundle:Pages:Events.html.twig');
     }
 
 }
